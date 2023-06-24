16b4ec34341659dc36b4a8e331f078ca48ce8bbd
abhishek-niraj <133279153+abhishek-niraj@users.noreply.github.com>
2023-06-24 20:17:38 +0530

    firstcommit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+function Homepage() {
+  return (
+    <div>
+      <PageNav></PageNav>
+      <h1>Worldwise</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Homepage;
